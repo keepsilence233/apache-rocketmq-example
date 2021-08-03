48821a3ee0c76caeab4b5248811d27e17d403df2
leizige <lei.tl@bitsun-inc.com>
2021-08-03 15:17:32 +0800

    send message

---
+++ rocketmq-common/src/main/java/common/MqConstants.java
@@ -0,0 +1,11 @@
+package common;
+
+public class MqConstants {
+
+
+    public final static String TEST_TOPIC = "test_topic";
+
+    public final static String TAG_A = "tag_a";
+    public final static String TAG_B = "tag_b";
+
+}
--- rocketmq-producer/src/main/java/qx/leizige/controller/ProducerController.java
+++ rocketmq-producer/src/main/java/qx/leizige/controller/ProducerController.java
@@ -1,10 +1,18 @@
 package qx.leizige.controller;
 
+import com.alibaba.fastjson.JSON;
+import common.MqConstants;
 import core.TracingRocketMqTemplate;
+import module.Item;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping(value = "/producer")
@@ -16,6 +24,23 @@ public class ProducerController {
     @GetMapping(value = "/send")
     public void send() {
 
+        Item item = new Item(1, "iphone", BigDecimal.TEN);
+
+        Message message = new Message(MqConstants.TEST_TOPIC, MqConstants.TAG_A, "KEY:" + 10086, JSON.toJSONBytes(item));
+        rocketMqTemplate.asyncSend(destination(), message, new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                System.out.printf("%s%n", sendResult);
+            }
+
+            @Override
+            public void onException(Throwable e) {
+                e.printStackTrace();
+            }
+        });
     }
 
+    private String destination() {
+        return MqConstants.TEST_TOPIC + ":" + MqConstants.TAG_A;
+    }
 }
