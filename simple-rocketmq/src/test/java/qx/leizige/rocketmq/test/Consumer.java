b030dfd33e8a11f95323ecfccf49165f184b5909
“leizige” <qx_leizige@163.com>
2022-08-24 23:39:39 +0800

    rocketmq demo test

---
+++ simple-rocketmq/src/test/java/qx/leizige/rocketmq/test/Consumer.java
@@ -0,0 +1,44 @@
+package qx.leizige.rocketmq.test;
+
+import org.apache.rocketmq.acl.common.AclClientRPCHook;
+import org.apache.rocketmq.acl.common.SessionCredentials;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.RPCHook;
+
+import java.util.List;
+
+public class Consumer {
+
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("MY_TEST_CONSUMER_GROUP");
+
+        // 设置NameServer的地址
+        consumer.setNamesrvAddr("192.168.2.106:9876");
+
+        // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
+        consumer.subscribe("MY_TOPIC", "*");
+        // 注册回调实现类来处理从broker拉取回来的消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                // 标记该消息已经被成功消费
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 启动消费者实例
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+
+    static RPCHook getAclRPCHook() {
+        return new AclClientRPCHook(new SessionCredentials("RocketMQ", "12345678"));
+    }
+}
